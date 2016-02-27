9ad3c8ca4b9c09c1ee6d830a5b9d6caa3a36bbe3
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-02-27 19:04:25 +0000

    Initial Commit

---
+++ rpg-oop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpg-oop
+//
+//  Created by Vyacheslav Horbach on 27/02/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
